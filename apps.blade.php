3ca7b92be1ab57fb447e07f80fdf459efed16142
Reginebam <67832084+Reginebam@users.noreply.github.com>
2020-07-08 11:37:10 +0000

    add2

---
+++ apps.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{asset('assets/css/style1.css')}}" rel="stylesheet">
+    {{-- <title>@yield('title')</title>
+    @include('includes.styles')
+    @yield('css') --}}
+</head>
+
+<body>
+    {{-- <div id="preloader"></div> --}}
+    @include('partials.navbar')
+
+
+    @yield('contenta')
+
+    @include('partials.footer')
+
+  {{-- @include('includes.script') --}}
+
+    {{-- @yield('js')  --}}
+    
+</body>
+
+</html>
